852adbb74cee3aa74c5731d410572180d1253a55
tonglei <tonglei@qq.com>
2021-07-16 19:36:31 +0800

    update

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.background import BackgroundTask
 from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 import uvicorn
-from sweet import Sweet
 
 
 app = Starlette()  #debug=True
 app.mount('/report', StaticFiles(directory='report'))
 
 
-async def suite(data):
-    sweet = Sweet()
-    sweet.run(data)
-    del sweet
-
-
 @app.route('/')
 async def homepage(request):
     return FileResponse('index.html')
-
-
-@app.route('/suite', methods=['POST'])
-async def sweet(request):
-    data = await request.json() 
-    task = BackgroundTask(suite, data=data)    
-    message = {'code': '0', 'message': 'Sweet is running'}
-    return JSONResponse(message, background=task)
 
     
 if __name__ == '__main__':
--- start.py
+++ start.py
@@ -3,5 +3,5 @@ from sweet import Autotest
 
 sweet = Autotest('测试计划示例')
 
-sweet.run()
+sweet.run()  # 如果要启动某几个测试套件，则可以写： sweet.run('baidu', 'echo') 
 
